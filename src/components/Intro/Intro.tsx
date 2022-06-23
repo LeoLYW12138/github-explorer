27788e02671edae7a80092fb48f8c41cbcb455f2
LeoLYW12138 <leo62227@gmail.com>
2022-06-23 18:39:00 +0800

    WIP: intro reponsiveness

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,11 +1,13 @@
 import styles from "./Intro.module.css"
 
-const Intro = () => {
+function Intro() {
   return (
     <section className={styles.section}>
       <div className={styles.container}>
-        <img className={styles.logo} src="/logo.png" alt="logo of Github User Explorer" />
-        <main>
+        <div className={styles["logo-container"]}>
+          <img className={styles.logo} src="/logo.png" alt="logo of Github User Explorer" />
+        </div>
+        <main className={styles["main-container"]}>
           <h1 className={styles.title}>Github User Explorer</h1>
           <p className={styles.description}>
             is a simple web application that can look up the contribution of a Github user at ease
