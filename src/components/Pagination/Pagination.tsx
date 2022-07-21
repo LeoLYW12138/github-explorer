320e8bc4caa3a181464d9f4a6d591a4fd4275e0f
LeoLYW12138 <leo62227@gmail.com>
2022-07-21 20:53:57 +0800

    feat: scroll to page in pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react"
 import { ReactComponent as IconChevronDown } from "../../icons/IconChevronDown.svg"
 import styles from "./Pagination.module.css"
 
@@ -8,10 +9,39 @@ interface PaginationProps {
 }
 
 function Pagination({ numPages, currPage, onChangePage }: PaginationProps) {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
   const handleOnChange = (page: number) => {
     if (!onChangePage || page < 1 || page > numPages) return
 
     onChangePage(page)
+
+    if (!wrapperRef.current) return
+    const scrollLeft = wrapperRef.current.scrollLeft
+    const scrollWidth = wrapperRef.current.scrollWidth
+    const { width: wrapperWidth, left: wrapperLeft } = wrapperRef.current.getBoundingClientRect()
+    const { left: btnLeft } = wrapperRef.current.children[page - 1].getBoundingClientRect()
+
+    const relativeLeft = scrollLeft + (btnLeft - wrapperLeft)
+    const PADDING = 40
+    let scroll = 0
+    // case 1: larger than wrapperWidth
+    if (relativeLeft > wrapperWidth - PADDING) {
+      scroll = relativeLeft - PADDING
+    }
+
+    // case 2: smaller than wrapperWidth
+    if (relativeLeft <= wrapperWidth - PADDING) {
+      scroll = 0
+    }
+
+    // case 3: larger than scrollWidth - wrapperWidth
+    if (relativeLeft > scrollWidth - wrapperWidth + PADDING) {
+      scroll = scrollWidth - wrapperWidth
+    }
+
+    // console.log(relativeLeft, wrapperLeft, wrapperWidth, scroll)
+    wrapperRef.current.scrollTo({ left: scroll, behavior: "smooth" })
   }
 
   return (
@@ -22,7 +52,7 @@ function Pagination({ numPages, currPage, onChangePage }: PaginationProps) {
           className={`${styles.prev} ${currPage === 1 ? styles.disabled : ""}`}
           onClick={() => handleOnChange(currPage - 1)}
         />
-        <div className={`scrollbar ${styles.wrapper}`}>
+        <div ref={wrapperRef} className={`scrollbar ${styles.wrapper}`}>
           {Array(numPages)
             .fill(0)
             .map((_, i) => (
